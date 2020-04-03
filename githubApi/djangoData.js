3a58695826b438e7e3e05c0899e738a43adc9b29
machine-user3741 <63016962+machine-user3741@users.noreply.github.com>
2020-04-02 20:32:53 -0600

    Programatic github interaction on 2020-04-03 02:32:53.181680+00:00 (UTC)

--- githubApi/djangoData.js
+++ githubApi/djangoData.js
@@ -1,34 +1,3 @@
 var locationsObj = {
-  "retailLocations": [
-    {
-      "id": 1,
-      "name": "EB GAMES",
-      "address": "st albert",
-      "latLng": {
-        "lat": "53.658740",
-        "lng": "-113.631403"
-      },
-      "place_id": "ChIJb5L8ifkloFMRpPTftGhal2E"
-    },
-    {
-      "id": 2,
-      "name": "COSTCO",
-      "address": "st albert",
-      "latLng": {
-        "lat": "53.671373",
-        "lng": "-113.636485"
-      },
-      "place_id": "ChIJHzoDH_oloFMRoykr0q76h80"
-    },
-    {
-      "id": 3,
-      "name": "WALMART",
-      "address": "St. Albert Trail",
-      "latLng": {
-        "lat": "53.659146",
-        "lng": "-113.633990"
-      },
-      "place_id": "ChIJBXJ5ofkloFMRuySIVM2bBSQ"
-    }
-  ]
+  "retailLocations": []
 };
