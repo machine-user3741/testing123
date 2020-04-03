efac51e153af216d9925fb52a04abbba7ec5dcfe
machine-user3741 <63016962+machine-user3741@users.noreply.github.com>
2020-04-02 19:26:05 -0600

    Programatic github interaction on 2020-04-03 01:26:05.582218+00:00 (UTC)

---
+++ githubApi/djangoData.js
@@ -0,0 +1,24 @@
+var locationsObj = {
+  "retailLocations": [
+    {
+      "id": 1,
+      "name": "EB GAMES",
+      "address": "st albert",
+      "latLng": {
+        "lat": "53.658740",
+        "lng": "-113.631403"
+      },
+      "place_id": "ChIJb5L8ifkloFMRpPTftGhal2E"
+    },
+    {
+      "id": 2,
+      "name": "COSTCO",
+      "address": "st albert",
+      "latLng": {
+        "lat": "53.671373",
+        "lng": "-113.636485"
+      },
+      "place_id": "ChIJHzoDH_oloFMRoykr0q76h80"
+    }
+  ]
+};
