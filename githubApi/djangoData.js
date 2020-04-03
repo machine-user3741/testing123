759444d4c70b56370f3d3461b7db5887ed83a651
machine-user3741 <63016962+machine-user3741@users.noreply.github.com>
2020-04-02 23:41:39 -0600

    Programatic github interaction on 2020-04-03 05:41:38.359921+00:00 (UTC)

--- githubApi/djangoData.js
+++ githubApi/djangoData.js
@@ -1,3 +1,14 @@
 var locationsObj = {
-  "retailLocations": []
+  "retailLocations": [
+    {
+      "id": 87,
+      "name": "WALMART SUPERCENTRE",
+      "address": "700 St Albert Trail, St. Albert, AB T8N 7H5",
+      "latLng": {
+        "lat": "53.659146",
+        "lng": "-113.633990"
+      },
+      "place_id": "ChIJBXJ5ofkloFMRuySIVM2bBSQ"
+    }
+  ]
 };
