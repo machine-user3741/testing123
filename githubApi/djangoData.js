5590462959d08ccc89f33d68e69d487e457220d3
machine-user3741 <63016962+machine-user3741@users.noreply.github.com>
2020-04-25 15:54:20 -0600

    Programatic github interaction on 2020-04-25 21:54:20.184398+00:00 (UTC)

--- githubApi/djangoData.js
+++ githubApi/djangoData.js
@@ -9,26 +9,6 @@ var locationsObj = {
         "lng": -113.628425
       },
       "place_id": "ChIJ71KguiR26lIR-sldplQ-344"
-    },
-    {
-      "id": 92,
-      "name": "SOBEYS",
-      "address": "Edmonton",
-      "latLng": {
-        "lat": 53.453707,
-        "lng": -113.421961
-      },
-      "place_id": "ChIJrb_rqWoZoFMR81SNLoGxuUM"
-    },
-    {
-      "id": 93,
-      "name": "EB GAMES\\",
-      "address": "St albert",
-      "latLng": {
-        "lat": 53.658740,
-        "lng": -113.631403
-      },
-      "place_id": "ChIJb5L8ifkloFMRpPTftGhal2E"
     }
   ]
 };
