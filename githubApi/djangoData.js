860686695bfb56896f52536046cede2a21bf048e
machine-user3741 <63016962+machine-user3741@users.noreply.github.com>
2020-04-25 15:24:27 -0600

    Programatic github interaction on 2020-04-25 21:24:26.553246+00:00 (UTC)

--- githubApi/djangoData.js
+++ githubApi/djangoData.js
@@ -9,6 +9,16 @@ var locationsObj = {
         "lng": -113.628425
       },
       "place_id": "ChIJ71KguiR26lIR-sldplQ-344"
+    },
+    {
+      "id": 92,
+      "name": "SOBEYS",
+      "address": "Edmonton",
+      "latLng": {
+        "lat": 53.453707,
+        "lng": -113.421961
+      },
+      "place_id": "ChIJrb_rqWoZoFMR81SNLoGxuUM"
     }
   ]
 };
