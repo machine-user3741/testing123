6eae9b0e85f4967d3900b966bd9eeb75859aaff7
machine-user3741 <63016962+machine-user3741@users.noreply.github.com>
2020-04-26 00:28:20 -0600

    Programatic github interaction on 2020-04-26 06:28:19.544843+00:00 (UTC)

--- githubApi/djangoData.js
+++ githubApi/djangoData.js
@@ -9,6 +9,16 @@ var locationsObj = {
         "lng": -113.628425
       },
       "place_id": "ChIJ71KguiR26lIR-sldplQ-344"
+    },
+    {
+      "id": 94,
+      "name": "EB GAMES",
+      "address": "st albert",
+      "latLng": {
+        "lat": 53.658740,
+        "lng": -113.631403
+      },
+      "place_id": "ChIJb5L8ifkloFMRpPTftGhal2E"
     }
   ]
 };
